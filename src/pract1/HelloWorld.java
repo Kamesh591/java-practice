1a19584483318cb1f7a2450dd1d8f0c116b74e27
Kameswara Rao Gandru <kameswararao313209@gmail.com>
2022-11-11 07:11:58 +0530

    Committing first hava practice file

---
+++ src/pract1/HelloWorld.java
@@ -0,0 +1,57 @@
+package pract1;
+
+public class HelloWorld {
+
+    static int value1;
+    int num2;
+
+    public static  void main(String[] args){
+        //53 identifiers
+        //data type identifiers: byte , short, int , long , float, double, char & boolean
+
+        //static
+
+        byte b1=20;
+        short s1=250;
+        int i1=25000;
+        long l1=100000l;
+        float f1=10/3f;
+        double d1=10/3D;
+        char c1='\u2555';
+        boolean bo1=false;
+        System.out.println("Byte value is:"+b1);
+        System.out.println("Short value is:"+s1);
+        System.out.println("Int value is:"+i1);
+        System.out.println("Long value is:"+l1);
+        System.out.println("Float value is:"+f1);
+        System.out.println("Double value is:"+d1);
+        System.out.println("Char value is:"+c1);
+        System.out.println("Booleam value is:"+bo1);
+
+
+        //Comments
+
+       // System.out.println(sum);
+    }
+
+    //// The below method addition is used for adding 2 numbers
+    // /* */
+
+    /**
+     *
+     * @Author Kameswar Rao Gandru
+     * @purpose Addition of 2 integer number
+     */
+
+    public static void addition(int num1, int num2){
+        int sum;
+        sum=num1+num2;
+    }
+
+
+
+    //varibles
+
+    // a-z A-Z 0-9 $ _
+    // static variable, instance variable, local variable
+}
